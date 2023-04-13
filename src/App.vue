25e0b25d474f5281d2f598553097defabe2384cb
Yurii Rybachok <drummer2009@ukr.net>
2023-04-13 15:09:50 +0300

    create project & setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { omMounted, ref } from 'vue';
+
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
+import TrendingUp from 'vue-material-design-icons/TrendingUp.vue';
+import Television from 'vue-material-design-icons/Television.vue';
+import MovieOutline from 'vue-material-design-icons/MovieOutline.vue';
+import Plus from 'vue-material-design-icons/Plus.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+
+import { useMovieStore } from './stores/movie';
+import { storeToRefs } from 'pinia';
+const useMovie = useMovieStore()
+const { movie, showFullVideo } = storeToRefs(useMovie)
+</script>
+
+<template>
+<div class="text-red-900">
+  Text here
+</div>
+</template>
