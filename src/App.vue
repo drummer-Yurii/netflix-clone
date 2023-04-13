6317be5f26039a0619da0f19be784c0696f77fb1
Yurii Rybachok <drummer2009@ukr.net>
2023-04-13 15:22:03 +0300

    sideNav for main section

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { omMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 import Magnify from 'vue-material-design-icons/Magnify.vue';
 import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
@@ -16,7 +16,29 @@ const { movie, showFullVideo } = storeToRefs(useMovie)
 </script>
 
 <template>
-<div class="text-red-900">
-  Text here
+<div class="fixed w-full h-screen bg-black">
+  <div v-if="!showFullVideo" id="SideNav" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
+    IMAGE
+    <div>
+      <div class="py-2 mx-10 my-6">
+        <Magnify fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
+      </div>
+      <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
+        <HomeOutline fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
+      </div>
+      <div class="py-2 mx-10 my-6">
+        <TrendingUp fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
+      </div>
+      <div class="py-2 mx-10 my-6">
+        <Television fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
+      </div>
+      <div class="py-2 mx-10 my-6">
+        <MovieOutline fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
+      </div>
+      <div class="py-2 mx-10 my-6">
+        <Plus fillColor="#FFFFFF" :size="40" class="cursor-pointer" />
+      </div>
+    </div>
+  </div>
 </div>
 </template>
