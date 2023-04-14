9dc3b8bc7a7befceed2f1d10c3d6dee4ba81556a
Yurii Rybachok <drummer2009@ukr.net>
2023-04-14 11:03:39 +0300

    VideoCarousel component

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import MovieOutline from 'vue-material-design-icons/MovieOutline.vue';
 import Plus from 'vue-material-design-icons/Plus.vue';
 import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
 
+import VideoCarousel from '@/components/VideoCarousel.vue';
 import MovieDetails from '@/components/MovieDetails.vue';
 
 import { useMovieStore } from './stores/movie';
@@ -50,12 +51,20 @@ onMounted(() => {
 
     <div v-if="!showFullVideo">
       <div class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
-
+        <div class="absolute z-30 h-[600px] left-[120px] w-[77%] right-0 top-0 bg-gradient-to-r from-black via-black" />
         <MovieDetails v-if="movie" :movie="movie" />
 
         <video v-if="movie" :src="'/videos/' + movie.name + '.mp4'" autoplay loop
           class="absolute z-0 h-[600px] right-0 top-0" />
       </div>
+
+      <div class="fixed z-30 bottom-0 right-0 w-full h-[55%] pl-[120px] overflow-y-auto">
+        <VideoCarousel class="pb-14 pt-14" category="Popular Movies" :movies="movies[0]" />
+        <VideoCarousel class="pb-14" category="Horror Movies" :movies="movies[1]" />
+        <VideoCarousel class="pb-32" category="Featured Movies" :movies="movies[2]" />
+      </div>
     </div>
+
+    <div class="absolute z-20 h-[70%] left-[120px] w-[100%] right-0 bottom-0 bg-gradient-to-t from-black via-black" />
   </div>
 </template>
