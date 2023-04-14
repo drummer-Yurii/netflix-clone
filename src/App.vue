435bd357768f3d6241888481b38659c16f898e63
Yurii Rybachok <drummer2009@ukr.net>
2023-04-14 10:08:34 +0300

    MovieDetails component

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@ import MovieOutline from 'vue-material-design-icons/MovieOutline.vue';
 import Plus from 'vue-material-design-icons/Plus.vue';
 import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
 
+import MovieDetails from '@/components/MovieDetails.vue';
+
 import { useMovieStore } from './stores/movie';
 import { storeToRefs } from 'pinia';
 const useMovie = useMovieStore()
@@ -21,41 +23,39 @@ onMounted(() => {
 </script>
 
 <template>
-<div class="fixed w-full h-screen bg-black">
-  <div v-if="!showFullVideo" id="SideNav" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
-    <img class="absolute top-0 w-[35px] mt-5 ml-10" src="/images/netflix-logo.png">
-    <div class="mt-20">
-      <div class="py-2 mx-10 my-6">
-        <Magnify fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
-      </div>
-      <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
-        <HomeOutline fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
-      </div>
-      <div class="py-2 mx-10 my-6">
-        <TrendingUp fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
-      </div>
-      <div class="py-2 mx-10 my-6">
-        <Television fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
-      </div>
-      <div class="py-2 mx-10 my-6">
-        <MovieOutline fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
-      </div>
-      <div class="py-2 mx-10 my-6">
-        <Plus fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
+  <div class="fixed w-full h-screen bg-black">
+    <div v-if="!showFullVideo" id="SideNav" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
+      <img class="absolute top-0 w-[35px] mt-5 ml-10" src="/images/netflix-logo.png">
+      <div class="mt-20">
+        <div class="py-2 mx-10 my-6">
+          <Magnify fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
+          <HomeOutline fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <TrendingUp fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <Television fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <MovieOutline fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <Plus fillColor="#FFFFFF" :size="35" class="cursor-pointer" />
+        </div>
       </div>
     </div>
-  </div>
 
-  <div v-if="!showFullVideo">
-    <div class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
-      <video 
-        v-if="movie"
-        :src="'/videos/'+movie.name+'.mp4'"
-        autoplay
-        loop
-        class="absolute z-0 h-[600px] right-0 top-0"
-      />
+    <div v-if="!showFullVideo">
+      <div class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
+
+        <MovieDetails v-if="movie" :movie="movie" />
+
+        <video v-if="movie" :src="'/videos/' + movie.name + '.mp4'" autoplay loop
+          class="absolute z-0 h-[600px] right-0 top-0" />
+      </div>
     </div>
   </div>
-</div>
 </template>
